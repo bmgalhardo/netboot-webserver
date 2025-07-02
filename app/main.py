2ae119dcd4c5776049f73c56130730b22ab89846
Bruno Galhardo <bgalhardo@eneida.io>
2025-07-02 15:02:33 +0100

    testing new ipxe

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ def dynamic_ipxe(mac_addr: str) -> Response:
         script = f"""
             #!ipxe
             echo Booting {mac.as_colon()} with Talos
-            chain {settings.netboot_url}/static/talos/proxmox/talos.ipxe
+            chain {settings.netboot_url}/boot/talos
             """
     else:
         script = f"""
@@ -37,6 +37,21 @@ def dynamic_ipxe(mac_addr: str) -> Response:
             chain {settings.netboot_url}/static/alpine/alpine.ipxe
         """
 
+    return Response(content=textwrap.dedent(script).lstrip('\n'), media_type="text/plain")
+
+@app.get('/talos')
+def dynamic_talos_ipxe() -> Response:
+    # siderolink.api=https://omni.bgalhardo.local:8090?jointoken=eaiaoYVEiz0bPCAoqiNJrnSretcDCDEMYuelfAtHzRnC&grpc_tunnel=true talos.events.sink=[fdae:41e4:649b:9303::1]:8091 talos.logging.kernel=tcp://[fdae:41e4:649b:9303::1]:8092
+    script = f"""
+        #!ipxe
+        
+        set boot-url {settings.netboot_url}
+        set kernel-args talos.platform=metal console=tty0 init_on_alloc=1 slab_nomerge pti=on consoleblank=0 nvme_core.io_timeout=4294967295 printk.devkmsg=on ima_template=ima-ng ima_appraise=fix ima_hash=sha512 selinux=1
+
+        kernel ${{boot-url}}/static/talos/proxmox/kernel-amd64 ${{kernel-args}} talos.config=${{boot-url}}/static/talos/boot-assets.yml
+        initrd ${{boot-url}}/static/talos/proxmox/initramfs-amd64.xz
+        boot
+    """
     return Response(content=textwrap.dedent(script).lstrip('\n'), media_type="text/plain")
 
 if __name__ == '__main__':
